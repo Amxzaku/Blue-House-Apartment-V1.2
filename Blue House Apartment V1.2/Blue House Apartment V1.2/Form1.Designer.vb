73d0dde337e8ff5fc286a659f0bcfb5813f59360
Dan <zexion313@gmail.com>
2023-11-13 19:04:28 +0800

    Nag add ko code sa Master List na button ug sa Data Entry na button

--- Blue House Apartment V1.2/Blue House Apartment V1.2/Form1.Designer.vb
+++ Blue House Apartment V1.2/Blue House Apartment V1.2/Form1.Designer.vb
@@ -37,9 +37,10 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Impact", 36.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
-        Me.Label1.Location = New System.Drawing.Point(299, 9)
+        Me.Label1.Location = New System.Drawing.Point(399, 11)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(484, 60)
+        Me.Label1.Size = New System.Drawing.Size(596, 75)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Blue House Apartment"
         '
@@ -47,9 +48,10 @@ Partial Class Form1
         '
         Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Button1.Font = New System.Drawing.Font("Stencil", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(99, 72)
+        Me.Button1.Location = New System.Drawing.Point(132, 89)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(224, 61)
+        Me.Button1.Size = New System.Drawing.Size(299, 75)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Master List"
         Me.Button1.UseVisualStyleBackColor = True
@@ -58,9 +60,10 @@ Partial Class Form1
         '
         Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Button2.Font = New System.Drawing.Font("Stencil", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(329, 72)
+        Me.Button2.Location = New System.Drawing.Point(439, 89)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(224, 61)
+        Me.Button2.Size = New System.Drawing.Size(299, 75)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Data Entry"
         Me.Button2.UseVisualStyleBackColor = True
@@ -69,9 +72,10 @@ Partial Class Form1
         '
         Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Button3.Font = New System.Drawing.Font("Stencil", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.Location = New System.Drawing.Point(559, 72)
+        Me.Button3.Location = New System.Drawing.Point(745, 89)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(224, 61)
+        Me.Button3.Size = New System.Drawing.Size(299, 75)
         Me.Button3.TabIndex = 4
         Me.Button3.Text = "Payments"
         Me.Button3.UseVisualStyleBackColor = True
@@ -80,9 +84,10 @@ Partial Class Form1
         '
         Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Button4.Font = New System.Drawing.Font("Stencil", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button4.Location = New System.Drawing.Point(789, 72)
+        Me.Button4.Location = New System.Drawing.Point(1052, 89)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(224, 61)
+        Me.Button4.Size = New System.Drawing.Size(299, 75)
         Me.Button4.TabIndex = 3
         Me.Button4.Text = "Others"
         Me.Button4.UseVisualStyleBackColor = True
@@ -90,31 +95,34 @@ Partial Class Form1
         'Panel1
         '
         Me.Panel1.Controls.Add(Me.ListView1)
-        Me.Panel1.Location = New System.Drawing.Point(101, 142)
+        Me.Panel1.Location = New System.Drawing.Point(135, 175)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(911, 484)
+        Me.Panel1.Size = New System.Drawing.Size(1215, 596)
         Me.Panel1.TabIndex = 5
         '
         'ListView1
         '
         Me.ListView1.HideSelection = False
-        Me.ListView1.Location = New System.Drawing.Point(10, 10)
+        Me.ListView1.Location = New System.Drawing.Point(13, 12)
+        Me.ListView1.Margin = New System.Windows.Forms.Padding(4)
         Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(898, 460)
+        Me.ListView1.Size = New System.Drawing.Size(1196, 565)
         Me.ListView1.TabIndex = 0
         Me.ListView1.UseCompatibleStateImageBehavior = False
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1134, 641)
+        Me.ClientSize = New System.Drawing.Size(1512, 789)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Blue House Apartment"
         Me.Panel1.ResumeLayout(False)
